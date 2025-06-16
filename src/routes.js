5cdee2fe4230a22b18e50cb9725ede9551b72740
GustavoFSProg <gustavo.prog40@gmail.com>
2025-06-16 19:04:05 -0300

    favoritos pronto

--- src/routes.js
+++ src/routes.js
@@ -67,6 +67,9 @@ router.get("/banners", controllerBanner.Listar);
 // empresas
 router.get("/empresas/destaques",  jwt.ValidateToken, controllerEmpresa.Destaques);
 router.get("/empresas",  jwt.ValidateToken, controllerEmpresa.Listar);
+router.post("/empresas/:id_empresa/favoritos", jwt.ValidateToken, controllerEmpresa.InserirFavorito);
+router.delete("/empresas/:id_empresa/favoritos", jwt.ValidateToken, controllerEmpresa.ExcluirFavorito);
+// router.get("/empresas/:id_empresa/cardapio", jwt.ValidateToken, controllerEmpresa.Cardapio);
 
 
 // pedidos
